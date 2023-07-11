004151b769938e043364022609210615b2290c0b
Shah Hassan <shahhassancs@gmail.com>
2023-07-11 13:07:19 +0500

    started implementing tree view for todos

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,21 @@
 import * as vscode from 'vscode';
 import { WebviewPanel } from './WebviewPanel';
 import { SidebarProvider } from './SidebarProvider';
+import { NodeDependenciesProvider } from './TreeProvider';
+import { TestView } from './TreeView';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "todo-list" is now active!');
-
+	const rootPath =
+    vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+      ? vscode.workspace.workspaceFolders[0].uri.fsPath
+      : undefined;
+  const nodeDependenciesProvider = new NodeDependenciesProvider(rootPath ? rootPath : "");
+  vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
+  vscode.commands.registerCommand('nodeDependencies.refreshEntry', () =>
+    nodeDependenciesProvider.refresh()
+  );
 	
 	const sidebarProvider = new SidebarProvider(context.extensionUri);
 	context.subscriptions.push(
@@ -29,6 +39,8 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('todo-list.webviewPanel', () => {
 		WebviewPanel.createOrShow(context.extensionUri)
 	}))
+
+	new TestView(context);
 }
 
 export function deactivate() {}
